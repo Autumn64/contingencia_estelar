67deb3227b67a62b69425f1b07e7e997d56ba107
Autumn64 <autumn64@disroot.org>
2025-12-02 13:28:57 -0600

    Niveles
    Signed-off-by: Autumn64 <autumn64@disroot.org>

--- contingencia-estelar/scripts/hud.gd
+++ contingencia-estelar/scripts/hud.gd
@@ -1,10 +1,29 @@
 extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $"/root/Juego/AnimationPlayer"
+@onready var titulo: Label = $"/root/Juego/HUD/CenterContainer/Titulo"
+
+var time_elapsed: float = 0.0
+
+func reset_time():
+	time_elapsed = 0.0
 
 func fade_to_black():
-	if animation_player.is_playing(): return
+	animation_player.stop()
 	animation_player.play("fade_out")
+
+func level_change():
+	var level = Globals.levels[Globals.current_level]
+	level["completion_time"] = time_elapsed
+	print("Nivel " + str(Globals.current_level) + ": " + str(level["completion_time"]))
+	reset_time()
+	Globals.current_level += 1
+	show_label()
+	
+func show_label():
+	var level = Globals.levels[Globals.current_level]
+	titulo.text = level["label"]
+	animation_player.play("show_title")
 
 @onready var burbujas: Array = [
 	$"Burbujas/b-1",
@@ -47,3 +66,6 @@ func dibujar_burbujas(cant: int) -> void:
 	while i < 5:
 		burbujas[i].visible = false
 		i += 1
+
+func _process(delta: float) -> void:
+	time_elapsed += delta
