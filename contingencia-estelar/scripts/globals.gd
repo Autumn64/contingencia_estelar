057c9356421f11449e1015fc1255284c7f2b2e0f
Autumn64 <autumn64@disroot.org>
2025-12-05 15:58:50 -0600

    v1.0.0
    Signed-off-by: Autumn64 <autumn64@disroot.org>

--- contingencia-estelar/scripts/globals.gd
+++ contingencia-estelar/scripts/globals.gd
@@ -1,25 +1,25 @@
 extends Node
 
 var current_level: int = 1
-const time_limit: float = 30.0
+const time_limit: float = 120.0
 const points_factor = 125
 
 var levels := {
 	1: {
-		"label": "Capítulo 1: ¿?",
-		"position": Vector2(2770.0, 54.0),
+		"label": "Capítulo 1: ¿No regresarán  . . .   por mí?",
+		"position": Vector2(15.0, 77.0),
 		"completion_time": 0.0,
 		"points": 0
 	},
 	2: {
-		"label": "Capítulo 2:",
-		"position": Vector2(820.0, 95.0),
+		"label": "Capítulo 2: Debo regresar a casa .",
+		"position": Vector2(3399.0, 79.0),
 		"completion_time": 0.0,
 		"points": 0
 	},
 	3: {
-		"label": "Capítulo 3:",
-		"position": Vector2(1903.0, -2.0),
+		"label": "Capítulo 3: No parece haber fin  . . .",
+		"position": Vector2(6884.0, 15.0),
 		"completion_time": 0.0,
 		"points": 0
 	},
