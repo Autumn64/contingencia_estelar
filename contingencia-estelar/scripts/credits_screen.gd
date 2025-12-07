fd0ebdb75ae12102b40c4524a247efec51d88982
Autumn64 <autumn64@disroot.org>
2025-12-07 13:15:30 -0600

    v1.0.1
    Signed-off-by: Autumn64 <autumn64@disroot.org>

--- contingencia-estelar/scripts/credits_screen.gd
+++ contingencia-estelar/scripts/credits_screen.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("enter"):
 		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
